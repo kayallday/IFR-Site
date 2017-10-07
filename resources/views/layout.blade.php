9158dce2c57cd1609e5f725fa01d69f9b6ca34e4
kaybeard <kmbrickel@fullsail.edu>
2017-10-06 20:31:28 -0400

    initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,36 @@
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="../../../../favicon.ico">
+
+  <title>IFR - Lawyer</title>
+
+  <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  <!-- Font-awesome icons -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Custom styles for this template -->
+  <link href="../css/album.css" rel="stylesheet">
+</head>
+
+<body>
+
+  @include ('layouts.nav')
+
+  <!--<div class="container">-->
+
+    @yield ('content')
+
+  <!--</div>-->
+
+  @include ('layouts.social')
+
+  @include ('layouts.footer')
+
+  <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+  <script src="../../../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,60 @@
+@extends ('layout')
+
+@section ('nav')
+
+@endsection
+
+@section ('content')
+
+
+<div class="jumbotron">
+  <div class="container">
+    <h1 class="display-3">Children need Fathers. Not visitors.</h1>
+    <p>Find the support you need. Support a great cause. Join the Indiana Father's Rights movement.</p>
+    <p><a class="btn btn-primary btn-lg" href="#" role="button">Sign Up &raquo;</a></p>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-sm-4">
+    <div class="cards">
+      <img class="card-img-top" src="" alt="">
+      <div class="card-body">
+        <p class="card-text">It's always comforting to know you're not alone during difficult times, and when it comes to Family Court you can feel very isolated. Find support you need locally, or online today.</p>
+        <a href="#" class="btn btn-primary">SUPPORT</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="cards">
+      <img class="card-img-top" src="" alt="">
+      <div class="card-body">
+        <p class="card-text">
+          Find legal resources shouldn't require tons of effort. We pulled all the resources we could together and put them in one convenient place. Find the resources you need here.
+        </p>
+        <a href="#" class="btn btn-primary">LEGAL</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="cards">
+      <img class="card-img-top" src="" alt="">
+      <div class="card-body">
+        <p class="card-text">Time is our most precious commodity, we can never get it back. So why not spend it doing something that matters. Find out how you can help today.</p>
+        <a href="#" class="btn btn-primary">VOLUNTEER</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+@endsection
+
+@section ('social')
+
+@endsection
+
+
+@section ('footer')
+
+
+@endsection
