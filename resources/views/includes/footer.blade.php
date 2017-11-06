226a26a87dcb1ae837522636dac980daf371968c
kaybeard <kmbrickel@fullsail.edu>
2017-11-05 23:06:36 -0500

    setup: setting up dev environment and pages

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,13 @@
+<div id="footer-nav">
+     <ul>
+       <!-- <li><a href="#">Sitemap</a> |</li>
+       <li><a href="#">Privacy Policy</a> |</li> -->
+       <li><a href="#">Login</a> |</li>
+       <li><a href="#">SignUp</a></li>
+     </ul>
+     <p>Contact - <a href="mailto:indianafathersrights@gmail.com?Subject=Tell%20us%20your%20story" target="_top">indianafathersrights@gmail.com</a></p>
+   </div>
+   <div id ="copyright">
+     Indiana Father's Rights Organization &copy; 2017 | All Rights Reserved - <a href="https://www.facebook.com/inexxdesign/">Inexx Design</a>
+   </div>
+ </footer>
--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,35 +1,36 @@
+
 <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-  <a class="navbar-brand" href="#"><img src=""><span class="sr-only">Indiana Father's Rights</span></a>
+  <a class="navbar-brand logo" href="#"><img src=""><span class="sr-only">Indiana Father's Rights</span></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse justify-content-end" id="navbarsExampleDefault">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">HOME<span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">HOME<span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">ABOUT</a>
+        <a class="nav-link" href="/about">ABOUT</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">RESOURCES</a>
         <div class="dropdown-menu" aria-labelledby="dropdown01">
-          <a class="dropdown-item" href="#">Court Forms</a>
+          <a class="dropdown-item" href="/court-forms">Court Forms</a>
           <a class="dropdown-item" href="#">Legal Council</a>
           <a class="dropdown-item" href="#">Support</a>
         </div>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">GET INVOLVED</a>
+        <a class="nav-link" href="/get-involved">GET INVOLVED</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">STORE</a>
+        <a class="nav-link" href="/store">STORE</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">CONTACT</a>
+        <a class="nav-link" href="/contact">CONTACT</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Login / Sign Up</a>
+        <a class="nav-link" href="/login">Login / Sign Up</a>
       </li>
     </ul>
         <!-- <form class="form-inline my-2 my-lg-0">
