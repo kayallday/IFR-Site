0e2b27c6edc5372bcd1624331012fe853088f756
kaybeard <kmbrickel@fullsail.edu>
2017-11-06 20:15:04 -0500

    try: fix for gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -7,20 +7,65 @@ module.exports = function(grunt) {
   pkg: grunt.file.readJSON('package.json'),
   sass: {
     dist: {
-      files: {
-        'style/style.css' : 'sass/style.scss'
-      }
+      options: {
+        // style: 'compressed',
+        sourcemap: 'none',
+        noCache: true
+      },
+      files: [{
+        expand: true,
+        cwd: 'resources/assets/',
+        src: ['**/*.scss'],
+        dest: '../public/css',
+        ext: '.css'
+      }],
+    },
+  },
+  uglify: {
+    dynamic_mappings: {
+      files: [
+        {
+          expand: true,
+          cwd: '../public/css',
+          src: ['**/*.css'],
+          dest: '../public/css',
+          ext: '.min.css'
+        }
+      ]
+
     }
   },
+  cssmin:{
+    dist: {
+      files: [{
+        expand: true,
+        cwd: 'resources/assets/',
+        src: ['*.scss'],
+        dest: '../public',
+        ext: '.css'
+      }],
+    }
+  },
+  // sass_globbing: {
+  //
+  // },
+  concat: {
+
+  },
   watch: {
-    css: {
-      files: '**/*.scss',
-      tasks: ['sass','uglify']
+    dist: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
     }
   }
 });
 grunt.loadNpmTasks('grunt-contrib-sass');
-grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+grunt.loadNpmTasks('grunt-sass-globbing');
+grunt.loadNpmTasks('grunt-contrib-concat');
+// grunt.loadNpmTasks('grunt-contrib-uglify');
 grunt.loadNpmTasks('grunt-contrib-watch');
-grunt.registerTask('default',['watch']);
+grunt.registerTask('default',['concat','sass','cssmin','watch']);
 }
